cce1b985f6cd47f6d2513444a9669a68bdc11b12
Jason Swett <jason@mhcare.com>
2021-05-01 08:50:51 -0400

    Initial commit.

---
+++ template.rb
@@ -0,0 +1,23 @@
+gem_group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'capybara'
+  gem 'webdrivers'
+  gem 'faker'
+end
+
+initializer 'generators.rb', <<-CODE
+  Rails.application.config.generators do |g|
+    g.test_framework :rspec,
+      fixtures:         false,
+      view_specs:       false,
+      helper_specs:     false,
+      routing_specs:    false,
+      request_specs:    false,
+      controller_specs: false
+  end
+CODE
+
+after_bundle do
+  generate 'rspec:install'
+end
