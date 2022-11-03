3f391532e3b1bb55ae45c5b35af92729c0ae154c
Jason Swett <jason@mhcare.com>
2022-11-03 18:11:22 -0400

    Move file to inline.

--- template/template.rb
+++ template/template.rb
@@ -4,7 +4,31 @@ def source_paths
   [__dir__]
 end
 
-template "config/database.yml.tt", "config/database.yml", force: true
+code = <<-CODE
+default: &default
+  adapter: postgresql
+  encoding: unicode
+  pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
+  username: #{@app_name}
+  host: 127.0.0.1
+  port: 5433
+
+development:
+  <<: *default
+  database: #{@app_name}_development
+
+test:
+  <<: *default
+  database: #{@app_name}_test
+
+production:
+  <<: *default
+  database: #{@app_name}_production
+  username: #{@app_name}
+  password:
+CODE
+file "config/database.yml", code, force: true
+
 template "docker-compose.yml.tt", "docker-compose.yml"
 template "init.sql.tt", "init.sql"
 
