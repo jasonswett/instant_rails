3f41a95cb809de4c019636c72f979d77c18cc7e1
Jason Swett <jason@mhcare.com>
2022-11-03 09:11:33 -0400

    Get things apparently working.

--- init.sh
+++ init.sh
@@ -9,8 +9,6 @@ APP_NAME=$1
 mkdir $APP_NAME
 cd $APP_NAME
 
-cp ../Dockerfile .
-
 cp ../docker-compose.yml .
 fill_template docker-compose.yml
 
@@ -19,12 +17,11 @@ fill_template init.sql
 
 cp -R ../template template
 
-docker compose run -e INSTANT_RAILS_APP_NAME=$1 web \
-  rails new . \
+rails new . \
   -f \
   -T \
   -d postgresql \
   -m template/template.rb
 
-docker compose run web bin/initialize
-docker compose up
+docker compose up -d
+bin/setup
